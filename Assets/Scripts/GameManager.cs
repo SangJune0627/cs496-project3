248aec2b2ae544d42c6fd9926c7c4167e7975db3
Park Sang June <67790478+SangJune0627@users.noreply.github.com>
2021-01-19 00:48:09 +0900

    before merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,47 +20,47 @@ public class GameManager : MonoBehaviourPunCallbacks
     public Text scoreText;
     public Transform[] spawnPositions;
     public GameObject playerPrefab;
-    public GameObject ballPrefab;
+    // public GameObject ballPrefab;
 
-    private int[] playerScores;
+    // private int[] playerScores;
 
     private void Start()
     {
-        playerScores = new[] {0, 0};
-        SpawnPlayer();
+        // playerScores = new[] {0, 0};
+        // SpawnPlayer();
 
         if (PhotonNetwork.IsMasterClient) SpawnBall();
     }
 
-    private void SpawnPlayer()
-    {
-        var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
-        var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
+    // private void SpawnPlayer()
+    // {
+    //     var localPlayerIndex = PhotonNetwork.LocalPlayer.ActorNumber - 1;
+    //     var spawnPosition = spawnPositions[localPlayerIndex % spawnPositions.Length];
 
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition.position, Quaternion.identity);
-    }
+    //     PhotonNetwork.Instantiate(playerPrefab.name, spawnPosition.position, Quaternion.identity);
+    // }
 
     private void SpawnBall()
     {
         PhotonNetwork.Instantiate(ballPrefab.name, Vector2.zero, Quaternion.identity).GetComponent<Ball>();
     }
 
-    public override void OnLeftRoom()
-    {
-        SceneManager.LoadScene("Lobby");
-    }
+    // public override void OnLeftRoom()
+    // {
+    //     SceneManager.LoadScene("Lobby");
+    // }
 
-    public void AddScore(int playerNumber, int score)
-    {
-        playerScores[playerNumber - 1] += score;
-        
-        photonView.RPC("RPCUpdateScoreText", RpcTarget.All, playerScores[0].ToString(), playerScores[1].ToString());
-    }
+    // public void AddScore(int playerNumber, int score)
+    // {
+    //     // playerScores[playerNumber - 1] += score;
 
-    
-    [PunRPC]
-    private void RPCUpdateScoreText(string player1ScoreText, string player2ScoreText)
-    {
-        scoreText.text = $"{player1ScoreText} : {player2ScoreText}";
-    }
+    //     photonView.RPC("RPCUpdateScoreText", RpcTarget.All, playerScores[0].ToString(), playerScores[1].ToString());
+    // }
+
+
+    // [PunRPC]
+    // private void RPCUpdateScoreText(string player1ScoreText, string player2ScoreText)
+    // {
+    //     scoreText.text = $"{player1ScoreText} : {player2ScoreText}";
+    // }
 }
